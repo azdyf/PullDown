d571b18d1ea6eadfddf5729d302236f5cb76eb4f
azdyf <azdyf.fm@gmail.com>
2018-04-15 11:45:22 +0700

    Initial commit

---
+++ app/src/main/java/id/devcode/azdyf/pulldown/MainActivity.java
@@ -0,0 +1,17 @@
+package id.devcode.azdyf.pulldown;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void pullDownProject() {
+        int i = 1+1;
+    }
+}
